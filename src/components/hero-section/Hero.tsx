d8e9957c4a7db1a53bb5ab4e9fff11ad724d594a
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2023-12-14 19:29:30 +0100

    Hero section

--- src/components/hero-section/Hero.tsx
+++ src/components/hero-section/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <section className='bg-hero min-h-screen grid place-items-center text-white'>
-      <h1 className='md:text-3xl lg:text-5xl leading-6'>
+      <h1 className='md:text-3xl lg:text-5xl leading-6 relative -top-32 md:top-0'>
         <p> Building</p>
         <p> Innovative </p>
         <p className=' font-title italic font-[600] mt-1'>
