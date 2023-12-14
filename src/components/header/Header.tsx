80c100d669abfa80497ef168138dac03669ead59
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2023-12-14 19:23:20 +0100

    Hero section

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -13,7 +13,7 @@ const navItems: NavItems[] = [
 export default function Navbar() {
   return (
     <header role='banner'>
-      <div className='w-[95%] md:w-[75%] rounded-[32px] bg-black/20 backdrop-blur-[16px] px-4 md:px-16 h-[72px] flex items-center justify-between fixed top-[2rem] lg:top-[6rem] left-1/2 -translate-x-1/2'>
+      <div className='w-[95%] md:w-[70%] rounded-[32px] bg-black/20 backdrop-blur-[16px] px-4 md:px-16 h-[72px] flex items-center justify-between fixed top-[2rem] lg:top-[4rem] left-1/2 -translate-x-1/2'>
         <div className='logo text-white'>Logo</div>
         <nav className='flex items-center gap-3 md:gap-6'>
           <button className='flex-shrink-0 bg-cta hover:bg-ctahover transition duration-300 px-5 py-2 rounded-[12px]'>
