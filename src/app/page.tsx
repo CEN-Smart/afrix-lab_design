34f87a7826c5bd9cdc093510c64813336be0f076
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2023-12-18 05:29:36 +0100

    complete hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from '@/components/hero-section/Hero';
+import Webdesign from '@/components/web-design-section/Webdesign';
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <Webdesign />
     </>
   );
 }
