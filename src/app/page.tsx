817a4190add862df2800d4f8190114ba37017c89
Oghor Obiazi <oghor.obiazi@gmail.com>
2023-12-18 08:42:42 +0100

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Hero from '@/components/hero-section/Hero';
+import Footer from '@/components/footer-section/Footer';
+
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <Footer />
     </>
   );
 }
