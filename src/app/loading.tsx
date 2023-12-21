1c9f345afc585a1b2445443f81b1d0079f4a407f
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2023-12-21 09:41:51 +0100

    Add not-found and loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,16 @@
+// generate a complete loading component with tailwind classes
+
+export default function Loading() {
+  return (
+    <div className='loading__container'>
+      <div className='loading__content'>
+        <h2 className='mb-8'>Loading...</h2>
+        <div className='loading__dots'>
+          <div className='loading__dot'></div>
+          <div className='loading__dot'></div>
+          <div className='loading__dot'></div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,23 @@
+// Create a nice Not Found page with tailwindcss classes and a link to the home page with a custom image and text.
+
+import Link from 'next/link';
+
+const NotFound = () => {
+  return (
+    <div className='not__found--container'>
+      <div className='not__found--content'>
+        <h2>Oops! Page not found.</h2>
+        <h1 className=' text-[140px] md:text-[160px]'>404</h1>
+        <p>We can&apos;t find the page you&apos;re looking for.</p>
+        <Link
+          className='bg-[#e55039aa] inline-block px-6 py-3 rounded-[25px] mt-2 uppercase text-[#fff]  hover:bg-[#e55039] transition duration-300 ease-in-out'
+          href='/'
+        >
+          Go back home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
