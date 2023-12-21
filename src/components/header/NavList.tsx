08d0d22a0e01d07c5ac0d32a4b3d7dc148be560c
CEN-Smart <nnanna.echi018@uniport.edu.ng>
2023-12-21 12:31:16 +0100

    fix bug on the nav links

--- src/components/header/NavList.tsx
+++ src/components/header/NavList.tsx
@@ -24,18 +24,21 @@ export default function NavList({
   const pathname = usePathname();
   return (
     <>
-      <li
-        onClick={onClick ? onClick : onNavClick}
-        className={cn(
-          `list-none navigation__item font-[600]  px-4 py-2 flex gap-3 cursor-pointer ${className}`,
-          {
-            'translate-x-4 bg-[#54890533]/20 shadow-xl rounded-[12px] font-extrabold text-[#548905]':
-              pathname === navObj.link,
-          }
-        )}
-      >
-        <span>{index}</span>
-        <Link href={navObj.link}>{children}</Link>
+      <li className='list-none flex items-end justify-center'>
+        <Link
+          onClick={onClick ? onClick : onNavClick}
+          className={cn(
+            ` navigation__item font-[600]  px-4 py-2 cursor-pointer inline-block ${className}`,
+            {
+              'translate-x-4 bg-[#54890533]/20 shadow-xl rounded-[12px] font-extrabold text-[#548905]':
+                pathname === navObj.link,
+            }
+          )}
+          href={navObj.link}
+        >
+          <span className='mr-3'>{index}</span>
+          {children}
+        </Link>
       </li>
     </>
   );
